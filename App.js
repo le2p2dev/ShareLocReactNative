2d8d33d3b4a33e6690cd86fdd894a8518b27572b
Hugo DI PIAZZA <hugo.di-piazza@etu.unistra.fr>
2021-12-09 16:15:35 +0100

    Revert "[ADD] Adding package"
    This reverts commit 55e38d6536daed58468ebcf37a42481d427e4078.

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import MySplashScreen from './src/MySplashScreen';
 
 export default function App() {
   const [appIsReady, setAppIsReady] = useState(false);
@@ -11,31 +10,33 @@ export default function App() {
   useEffect(() => {
     async function prepare() {
       try {
-        await SplashScreen.hideAsync(); // masquage
+        await SplashScreen.preventAutoHideAsync(); // affichage
+
         // Chargement des fonts, appels API externes, etc...
         await Font.loadAsync({
-
           'Georama Black': require('./assets/fonts/Georama-Black.ttf')
         });
         // On fait une pause 2 sec pour simuler un chargement lent
         await new Promise(resolve => setTimeout(resolve, 2000));
 
       } catch (e) { console.warn(e); } finally {
+        await SplashScreen.hideAsync(); // masquage
         setAppIsReady(true);
 
       }
     } prepare();
   }, [appIsReady]);
 
-  return appIsReady ? (
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
       <View style={styles.container}>
-        <Text style={styles.title}>ShareLoc Menu</Text>
+        <Text style={{fontFamily: 'Georama Black', color: '#404040'}}>ShareLoc</Text>
         <StatusBar style="auto" />
       </View>
-  ) : (
-      <MySplashScreen />
   );
-
 }
 
 const styles = StyleSheet.create({
@@ -45,9 +46,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontFamily: 'Georama Black',
-    color: '#404040',
-    fontSize: 25,
-  }
 });
