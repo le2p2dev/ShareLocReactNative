10f961f1f281445d9f5af3e6d5601b0736b7a423
Stéphane LITT <stephane.litt@etu.unistra.fr>
2022-01-07 10:26:23 +0100

    [Add] Colocation component

--- screens/ColocationScreen.js
+++ screens/ColocationScreen.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+
+import Colocation from '../components/Colocation';
+
+const test = {
+  name: 'Titre de la colocation',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam elementum malesuada purus eget bibendum. Vivamus sollicitudin sem vel dolor eleifend fringilla. Cras sit amet scelerisque urna. Cras non lectus diam. In diam quam, scelerisque in ex id, venenatis lacinia dolor. Sed dictum neque eget rhoncus cursus.',
+  dateCreation: '6 janvier 2022',
+}
 
 export default function ColocationScreen() {
   return (
-    <View>
-      <Text>My colocation</Text>
+    <View style={styles.container}>
+      <Colocation name={test.name} description={test.description} dateCreation={test.dateCreation} />
+      <Colocation name={test.name} description={test.description} dateCreation={test.dateCreation} />
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#aaf0d1',
+    alignItems: 'center',
+    padding: 24,
+  },
+});
